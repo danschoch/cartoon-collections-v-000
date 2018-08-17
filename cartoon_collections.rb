0aa1bbdc7ab7ffd786fe76fc0f85744e3aed8b9b
Daniel Schoch <d.schoch90@gmail.com>
2018-08-17 19:22:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ def summon_captain_planet(calls)
   calls.collect {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (calls)
+  calls.any?{|call| call.length > 4}
   # Your code here
 end
 
